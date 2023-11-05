9c87af7554710e454de653b4767a3f2d77edabd0
shawn edgell <shawn_edgell@hotmail.com>
2023-11-04 21:19:20 -0400

    added routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+// wiki.js - Wiki route module.
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const express = require("express");
+const router = express.Router();
+
+// Home page route.
+router.get("/", function (req, res) {
+  res.send("Wiki home page");
+});
+
+// About page route.
+router.get("/about", function (req, res) {
+  res.send("About this wiki");
 });
 
 module.exports = router;
